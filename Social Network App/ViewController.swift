0ace20fa065e525862e6b7da3a24df86c2f1fcb9
Alex-Besson <alex_j67@hotmail.com>
2015-11-13 10:59:51 -0500

    Initial commit

---
+++ Social Network App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social Network App
+//
+//  Created by Alexander Besson on 2015-11-13.
+//  Copyright © 2015 Alexander Besson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
